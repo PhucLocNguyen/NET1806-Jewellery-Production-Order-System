83cd78059120f4db1a511bb3f95b346e39fcc24e
NguyenBaDat <bmbadat2234@gmail.com>
2024-05-28 08:19:46 +0700

    New Jewellery Project

---
+++ BE/API/Controllers/StonesController.cs
@@ -0,0 +1,66 @@
+﻿using API.Model.StonesModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Repository;
+using Repository.Entity;
+using System.Drawing;
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StonesController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public StonesController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStonesById(int id)
+        {
+            var Stones = _unitOfWork.StoneRepository.GetByID(id);
+            return Ok(Stones);
+        }
+        [HttpPost]
+        public IActionResult CreateStones(RequestCreateStonesModel requestCreateStonesModel)
+        {
+            var Stones = new Stones()
+            {
+                Kind = requestCreateStonesModel.Kind,
+                Price = requestCreateStonesModel.Price,
+                Quantity = requestCreateStonesModel.Quantity,
+                Size = requestCreateStonesModel.Size,
+            };
+            _unitOfWork.StoneRepository.Insert(Stones);
+            _unitOfWork.Save();
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateStones(int id, RequestCreateStonesModel requestCreateStonesModel)
+        {
+            var existedStonesUpdate = _unitOfWork.StoneRepository.GetByID(id);
+            if(existedStonesUpdate == null)
+            {
+                return NotFound();
+            }
+            existedStonesUpdate.Kind = requestCreateStonesModel.Kind;
+            existedStonesUpdate.Price = requestCreateStonesModel.Price;
+            existedStonesUpdate.Quantity = requestCreateStonesModel.Quantity;
+            existedStonesUpdate.Size = requestCreateStonesModel.Size;
+            _unitOfWork.StoneRepository.Update(existedStonesUpdate);
+            _unitOfWork.Save();
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteStones(int id)
+        {
+            var existedStonesUpdate = _unitOfWork.StoneRepository.GetByID(id);
+            _unitOfWork.StoneRepository.Delete(existedStonesUpdate);
+            _unitOfWork.Save();
+            return Ok();
+        }
+    }
+}
---
+++ BE/Repository/Entity/Stones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Repository.Entity
+{
+    public partial class Stones
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StonesId { get; set; }
+        [Required]
+        public string Kind { get; set; }
+        [Required]
+        public string Size { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+        public virtual ICollection<Design> Designs { get; set; } = new List<Design>();
+    }
+
+}
