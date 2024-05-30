ef42e3bd69075b690dbaf5ffdf3d9ef29b90ab5a
NguyenDHung <hn19092003@gmail.com>
2024-05-30 10:56:10 +0700

    Upload Home Page

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,10 +1,11 @@
 import Login from './component/login/Login.jsx'
+import Home from './component/home/Home.jsx'
 import './App.css'
 
 function App() {
 
   return (
-    <Login></Login>
+    <Home></Home>
   )
 }
 
